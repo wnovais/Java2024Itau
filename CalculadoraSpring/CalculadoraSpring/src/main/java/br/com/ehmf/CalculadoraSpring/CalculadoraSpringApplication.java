33c06e7a1c4fa26636be749eda105950435ae5f7
Willian Novais <willian.novais@hotmail.com>
2024-02-20 11:41:06 -0300

    Aula 19_02_2024 iniciado o spring

---
+++ CalculadoraSpring/CalculadoraSpring/src/main/java/br/com/ehmf/CalculadoraSpring/CalculadoraSpringApplication.java
@@ -0,0 +1,19 @@
+package br.com.ehmf.CalculadoraSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CalculadoraSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraSpringApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
---
+++ mavenJavaItau/src/test/java/br/com/ehmf/AppTeste.java
@@ -0,0 +1,12 @@
+package br.com.ehmf;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTeste {
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+}
